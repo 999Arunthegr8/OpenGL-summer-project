22eaf1e7aff11ebc8fdb66d88ac4bb3ca84e0b35
Eyuzik Chhunju <149344355+Eyuzikfr@users.noreply.github.com>
2025-03-23 09:03:26 +0545

    add ball movement and collision detection

--- PongPossibly/main.cpp
+++ PongPossibly/main.cpp
@@ -1,6 +1,8 @@
 #include <GL/glut.h>
 #include <iostream>
 #include <cmath>
+#include <sstream>
+#include <stdbool.h>
 
 const float PI = 3.14159265358979323846f;
 
@@ -8,6 +10,26 @@ const float PI = 3.14159265358979323846f;
 float paddleX = -0.9f, paddleY = 0.0f;
 const float paddleWidth = 0.04f, paddleHeight = 0.2f;
 const float paddleSpeed = 0.05f;
+
+//ball properties
+float ballX = 0.0f, ballY = 0.0f;
+float ballRadius = 0.025f;
+float ballSpeed = 0.01f;
+float maxSpeed = 0.03f;
+float ballDirX = -1.0f, ballDirY = 0.5f;
+
+//game functions
+int score = 0;
+bool isGameOver = false;
+
+void drawText(const char* text, float x, float y) {
+    glColor3f(1.0, 1.0, 1.0);  // White text
+    glRasterPos2f(x, y);        // Set position
+
+    for (int i = 0; text[i] != '\0'; i++) {
+        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, text[i]); // Render each character
+    }
+}
 
 static void onePlayerMode()
 {
@@ -25,14 +47,25 @@ static void onePlayerMode()
 
 	glColor3f(1.0, 1.0, 1.0);
 	glBegin(GL_TRIANGLE_FAN);
-	glVertex2f(0, 0);  // Center of the circle
+	glVertex2f(ballX, ballY);  // Center of the circle
 	for (int i = 0; i <= 50; i++) {
 		float angle = 2.0f * PI * i / 50;
-		float x = 0 + 0.025 * cos(angle);
-		float y = 0 + 0.025 * sin(angle);
+		float x = ballX + ballRadius * cos(angle);
+		float y = ballY + ballRadius * sin(angle);
 		glVertex2f(x, y);
 	}
 	glEnd();
+	
+	std::ostringstream scoreText;
+    scoreText << "Score: " << score;
+    drawText(scoreText.str().c_str(), -0.1f, 0.9f); // Position at top center
+    
+    if (isGameOver)
+    {
+    	std::ostringstream gameOverText;
+    	gameOverText << "Game Over!";
+    	drawText(gameOverText.str().c_str(), -0.15f, 0.7f);
+	}
 
 	glutSwapBuffers();
 }
@@ -50,7 +83,7 @@ void keyboard(unsigned char key, int x, int y)
 	switch (key)
 	{
 	case 'w':
-		if (paddleY < 1)
+		if (paddleY + paddleHeight < 1)
 		{
 			std::cout << "move up" << std::endl;
 			paddleY += paddleSpeed;
@@ -58,7 +91,7 @@ void keyboard(unsigned char key, int x, int y)
 		break;
 
 	case 's':
-		if (paddleY - 0.1 > -1)
+		if (paddleY > -1)
 		{
 			std::cout << "move down" << std::endl;
 			paddleY -= paddleSpeed;
@@ -67,10 +100,52 @@ void keyboard(unsigned char key, int x, int y)
 	}
 }
 
+void update()
+{
+	ballX += ballSpeed * ballDirX;
+	ballY += ballSpeed * ballDirY;
+	
+	// top and bottom wall detection
+	if(ballY + ballRadius >= 1.0f || ballY - ballRadius <=-1.0f)
+	{
+		ballDirY = -ballDirY;
+	}
+	
+	
+	// collision detection
+	if (ballX - ballRadius <= paddleX + paddleWidth &&
+		ballY >= paddleY && ballY <= paddleY + paddleHeight)
+	{
+		ballDirX = -ballDirX;
+		if (ballSpeed < maxSpeed)
+		{
+			ballSpeed += 0.0025f;
+		}
+		score++;
+	}
+	
+	// right wall detection
+	if (ballX + ballRadius >= 1.0f)
+	{
+		ballDirX = -ballDirX;
+	}
+	
+	if (ballX - ballRadius <= -1.0f)
+	{
+		isGameOver = true;
+	}
+
+	glutPostRedisplay();
+}
+
 void timer(int)
 {
-	glutPostRedisplay();
-	glutTimerFunc(1000 / 60, timer, 0);
+//	glutPostRedisplay();
+	update();
+	if (!isGameOver)
+	{
+		glutTimerFunc(1000 / 60, timer, 0);
+	}
 }
 
 int main(int argc, char** argv)
@@ -87,6 +162,7 @@ int main(int argc, char** argv)
 	glutDisplayFunc(onePlayerMode);
 
 	glutKeyboardFunc(keyboard);
+	
 	glutTimerFunc(0, timer, 0);
 
 	glutMainLoop();
